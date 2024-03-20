928ead6e6376b92fdafc4ed35ba8cb1039e17d47
Healoz <lauren.easter59@gmail.com>
2024-03-20 18:57:16 +1100

    creating folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './assets/App.css';
+
+// define object shape
+interface Data {
+  name: string;
+  age: number;
+  numbers: number[];
+}
 
 function App() {
+
+  const [data, setData] = useState<Data>({
+    name: 'Brian',
+    age: 0,
+    numbers: [3, 5, 6, 7, 10],
+  });
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>{data.name}</h1>
+      <p>Age: {data.age}</p>
+      <ul>
+        {data.numbers.map((num, index) => (
+          <li key={index}>{num}</li>
+        ))}
+      </ul>
     </div>
   );
 }
