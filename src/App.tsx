a0f4defda9a7fa5db24d51588f7e42a430c07a4f
Healoz <lauren.easter59@gmail.com>
2024-03-21 09:35:54 +1100

    created nav menu, switched to just css for managing responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,9 @@ import Footer from './components/Footer/Footer';
 
 function App() {
 
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-
-  // checking if screen is mobile
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    }
-
-    // Add event kusteber for window resize
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    // cleanup the eventn listener when component unmounts
-    return () => window.removeEventListener('resize', handleResize);
-
-  }, []);
-
   return (
     <div className="App">
-      <Header 
-        isMobile={isMobile}
-      />
+      <Header />
       
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,31 @@ import styles from './Header.module.css';
 import React from 'react';
 
 interface HeaderProps {
-    isMobile: boolean;
+    // isMobile: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ isMobile }) => {
+const Header: React.FC<HeaderProps> = ({ }) => {
 
     return (
         <section className={styles.header}>
-            <div className={styles.logo}>
-                <button>
-                    <span className={`material-symbols-outlined ${styles.icon}`}>garage_home</span>
-                    {!isMobile &&
+            <div className={styles.menuItems}>
+                <div className={styles.logo}>
+                    <button>
+                        <span className={`material-symbols-outlined ${styles.icon}`}>garage_home</span>
                         <h3>GlamGarage</h3>
-                    }
-                </button>
+                    </button>
+                </div>
+                    <ul className={styles.desktopMenuItems}>
+                        <li>Menu Item 1</li>
+                        <li>Menu Item 2</li>
+                        <li>Menu Item 3</li>
+                    </ul>
             </div>
+            
             <div className={styles.menuBtns}>
-                <button><span className={`material-symbols-outlined ${styles.iconSmall}`}>search</span></button>
+                <div className={styles.searchBar}>
+                    <button className={styles.searchBarBtn}><span className={`material-symbols-outlined ${styles.iconSmall} ${styles.searchBarIcon}`}>search</span></button>
+                </div>
                 <button><span className={`material-symbols-outlined ${styles.iconSmall}`}>shopping_cart</span></button>
             </div>
         </section>
