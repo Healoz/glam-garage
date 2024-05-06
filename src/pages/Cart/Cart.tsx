c9db6072bf1beb4c9ff295a5b9623454384b88f4
Healoz <lauren.easter59@gmail.com>
2024-05-06 11:39:59 +1000

    created base for mobile cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Button from "../../components/Button/Button";
 import { Product } from "../../data/types";
 import CartItem from "../../components/CartItem/CartItem";
+import ColourScheme from "../../enums/ColourScheme";
 
 interface CartProps {
   cartItems: Product[];
@@ -11,15 +12,40 @@ interface CartProps {
 const Cart: React.FC<CartProps> = ({ cartItems }) => {
   return (
     <main className={styles.cartPage}>
-      <h1>Your Cart</h1>
-      <div className={styles.cartGrid}>
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+      <div className={styles.cartGridContainer}>
+        <h1>Your Cart</h1>
+        <div className={styles.cartGrid}>
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </div>
       </div>
       <div className={styles.totalBox}>
-        <h3>Total</h3>
+        <h2 className={styles.totalTitle}>Total</h2>
+        <div className={styles.totalGrid}>
+          <div>
+            <h4>Sub-total</h4>
+          </div>
+          <div className={styles.price}>
+            <h2>$283</h2>
+          </div>
+          <div>
+            <h4>Shipping</h4>
+          </div>
+          <div className={styles.price}>
+            <h2>$27</h2>
+          </div>
+        </div>
+        <Button
+          buttonText="Checkout"
+          colourScheme={ColourScheme.Primary}
+          iconCode="arrow_forward"
+          buttonLink="/"
+          isCircle={false}
+          fillsSpace={true}
+        />
+        <p>Lorem ipsum dolor sit amet</p>
       </div>
     </main>
   );
